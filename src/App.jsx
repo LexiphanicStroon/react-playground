c2b315213277451daab2647e9f025dba15830531
LexiphanicStroon <nate_reeves@outlook.com>
2024-02-03 16:08:32 -0600

    update purpose of App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React, { useState } from "react";
 import "./App.css";
-import "./Greeting.css"
-import { SayHello } from "./Greeting.jsx";
 
-function App() {  
+
+function App() {
+
+  const [stateValue, setStateValue] = useState(initialValue);
+
+  const [backgroundColor, setBackgroundColor] = useState(initialColor);
+
   return (
     <>
-<SayHello />
+
     </>
-  );
+  )
 }
 
 export default App;
--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,27 +1,7 @@
-function Button(props) {
-const buttonStyle = {
-    color: props.color,
-    fontSize: props.fontSize + 'px'
-};
-return (
-    <button style={buttonStyle}>{props.text}</button>
-);
+function Greeting() {
+    
+
+
 }
 
-Button.defaultProps = {
-    text: "Click Me!",
-    color: "blue",
-    fontSize: 12
-  };
-
-function SayHello() {     
-    return (
-        <div>
-            <Button />
-            <Button text="Button Two" color="blue" fontSize="14" />
-            <Button text="Button Three" color="aqua" fontSize="30" />            
-        </div>
-    )
-}
-
-export {SayHello};
+export { Greeting as SayHello }
